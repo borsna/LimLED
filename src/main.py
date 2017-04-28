99a5ea56e01b3477188c94d41b4dabe9de564c31
Olof Olsson <borsna@gmail.com>
2017-04-28 20:31:04 +0200

    fix formatting

--- src/main.py
+++ src/main.py
@@ -2,10 +2,11 @@ import socket
 import sys
 import time
 
+#
 #docs:
 # http://www.limitlessled.com/dev/
 
-host = "10.10.10.255" #broadcast ip for my subnet
+host = "10.10.10.255" # broadcast ip for my subnet
 port = 8899
 
 #            all 1   2   3   4
@@ -15,12 +16,13 @@ try:
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.connect((host, port))
 except socket.error:
-    print ("Failed to create socket")
+    print("Failed to create socket")
     sys.exit()
+
 
 def main():
     if len(sys.argv) == 1:
-        print("arugments: [group] [command] [value]")
+        print("arguments: [group] [command] [value]")
         print("0 color 45   set color value for group all to 45")
     else:
         g = int(sys.argv[1])
@@ -38,22 +40,27 @@ def main():
         else:
             print("group must be 0-4 (0 send command to all)")
 
+
 def net_send(group, mode, value):
+    """Broadcast command"""
     try:
         s.send(bytearray([group]))
         time.sleep(0.1)
         s.send(bytearray([mode, value, 85]))
     except socket.error as e:
-        print ('Error Code : ' + str(e[0]) + ' Message ' + e[1])
+        print('Error Code : ' + str(e[0]) + ' Message ' + e[1])
         sys.exit()
+
 
 def brightness(group, brightness):
     """set brightness 2-27"""
     net_send(group, 78, brightness)
 
+
 def color(group, color):
     """set color 0-255"""
     net_send(group, 64, color)
+
 
 if __name__ == "__main__":
     main()
